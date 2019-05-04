0ffc43da73f6027424463f73f54a7a22d85567c7
Michael Miranda <42720791+mrpineapples@users.noreply.github.com>
2019-05-03 23:48:43 -0400

    refactor with transform data

--- src/components/GameSchedule.js
+++ src/components/GameSchedule.js
@@ -18,6 +18,48 @@ class GameSchedule extends Component {
       .catch(err => console.log(err))
   }
   
+  _transformData = () => {
+    // console.log(this.state.data.series)
+    let bracketSeries = this.state.data.series.map(round => {
+      
+      // _addSeriesIdToEachGame
+      let seriesId = round.series.id
+      let newGames = round.games.map(game => {
+
+        let awayTeam = game.teams.away.team.teamName
+        let homeTeam = game.teams.home.team.teamName
+
+        let winningPitcher = game.decisions.winner.initLastName
+        let winnerUrlSlug = game.decisions.winner.nameSlug
+        let losingPitcher = game.decisions.loser.initLastName
+        let loserUrlSlug = game.decisions.loser.nameSlug
+        let savePitcher = game.decisions.save ? game.decisions.save.initLastName : null;
+        let saveUrlSlug = game.decisions.save ? game.decisions.save.nameSlug : null;
+
+        let data = { 
+          ...game, 
+          seriesId,
+          awayTeam,
+          homeTeam,
+
+          winningPitcher,
+          winnerUrlSlug,
+          losingPitcher,
+          loserUrlSlug,
+          savePitcher,
+          saveUrlSlug
+        }
+    
+        return data
+
+      })
+      round.games = newGames
+      return round
+    })
+
+    return bracketSeries
+  }
+
   render() {
     if (this.state.data === null) {
       return null
@@ -38,24 +80,28 @@ class GameSchedule extends Component {
     // My thought here was to make an array with all the game summaries (35) and then filter the ones needed per game day. Components are just objects in the end
     let summaries = games.map(game => 
       <GameSummary
+        sortBy={this.props.sort}
+        // seriesMetaData={...}
         key={game.gamePk}
         gameId={game.gamePk}
         seriesStatus={`${game.seriesStatus.shortDescription} - ${game.seriesStatus.result}`}
+        round={game.seriesDescription}
+        final={game.linescore.scheduledInnings !== game.linescore.currentInning ? `F/${game.linescore.currentInning}`: "FINAL"}
+        date={formatDate(game.gameDate)}
+        broadcast={game.broadcasts.find(obj => obj.isNational).callSign}
+        // awayTeamData={....}
         awayTeam={game.teams.away.team.teamName}
         awayScore={game.teams.away.score}
+        // homeTeamData={...}
         homeTeam={game.teams.home.team.teamName}
         homeScore={game.teams.home.score}
-        broadcast={game.broadcasts.find(obj => obj.isNational).callSign}
+        // decisions={...}
         winningPitcher={game.decisions.winner.initLastName}
         winnerUrlSlug={game.decisions.winner.nameSlug}
         losingPitcher={game.decisions.loser.initLastName}
         loserUrlSlug={game.decisions.loser.nameSlug}
         savePitcher={game.decisions.save ? game.decisions.save.initLastName : null}
         saveUrlSlug={game.decisions.save ? game.decisions.save.nameSlug : null}
-        final={game.linescore.scheduledInnings !== game.linescore.currentInning ? `F/${game.linescore.currentInning}`: "FINAL"}
-        date={formatDate(game.gameDate)}
-        round={game.seriesDescription}
-        sortBy={this.props.sort}
       />
     )
     
